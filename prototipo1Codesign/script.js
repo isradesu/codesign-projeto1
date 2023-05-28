70aec41045dd70db2a3c386235d863ad7e70cef8
GadWitczak <119819421+GadWitczak@users.noreply.github.com>
2023-05-28 18:00:49 -0300

    Atualização dos botões

--- prototipo1Codesign/script.js
+++ prototipo1Codesign/script.js
@@ -2,5 +2,5 @@ function voltareScrollar(){
    window.location.href="presentation.html";
 }
 function scrollarprabaixo(){
-   scrollTo(0, (document.body.scrollHeight)-100)
+   scrollTo(0, document.body.scrollHeight);
 }
