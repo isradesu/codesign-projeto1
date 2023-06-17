c093cdb1e46b3fe58c073ebd1f3a11e4bee6b7a2
GadWitczak <119819421+GadWitczak@users.noreply.github.com>
2023-06-17 20:35:10 -0300

    aprimoramente
    tela de trilhas

--- prototipo1Codesign/scripts/trails.js
+++ prototipo1Codesign/scripts/trails.js
@@ -4,18 +4,22 @@ function mudartrilha(trilha){
     console.log("Você está na trilha", ntrilha);
     if(ntrilha==1){
       corujadastrilhas.setAttribute("src","../source/coruja design.png");
+      corujadastrilhas.style.marginTop="1vw";
+      corujadastrilhas.style.marginLeft="7vw";
     }
     if(ntrilha==2){
       corujadastrilhas.setAttribute("src","../source/coruja audiovisual.png");
+      corujadastrilhas.style.marginTop="2vw";
+      corujadastrilhas.style.marginLeft="6.8vw";
     }
     if(ntrilha==3){
       corujadastrilhas.setAttribute("src","../source/corujaprog.png");
+      corujadastrilhas.style.marginTop="1vw";
+      corujadastrilhas.style.marginLeft="6.25vw";
     }
     if(ntrilha==4){
       corujadastrilhas.setAttribute("src","../source/coruja jogos.png");
-      corujadastrilhas.style.marginTop="-2.5vw";
-      corujadastrilhas.style.marginLeft="-0.5vw";
-      corujadastrilhas.style.width="27.66453333vw";
-      corujadastrilhas.style.height="33.99375vw";
+      corujadastrilhas.style.marginTop="-3.8vw";
+      corujadastrilhas.style.marginLeft="-0.8vw";
     }
 }
